438437abafa031242a0a7a368dbf9660a67a8033
hoabao0809 <hoabao.0809@gmail.com>
2022-04-17 16:04:30 +0700

    create Navbar component

---
+++ src/Components/NavbarComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class NavbarComponent extends Component {
+  render() {
+    return (
+      <Navbar className="container-fluid" dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+        </div>
+      </Navbar>
+    );
+  }
+}
+
+export default NavbarComponent;
