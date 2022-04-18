12d0ea94c8ee39990926d73fc698bf166c89a84d
hoabao0809 <hoabao.0809@gmail.com>
2022-04-18 16:29:56 +0700

    handle Event switch display columns

--- src/Components/StaffList.js
+++ src/Components/StaffList.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './StaffList.css';
+import dateFormat from 'dateformat';
 
 class StaffList extends Component {
   constructor(props) {
@@ -7,21 +8,102 @@ class StaffList extends Component {
 
     this.state = {
       selectedStaff: null,
+      colDisplay: 'col-lg-4',
     };
   }
-  render() {
-    const staffList = this.props.staffs.map((staff) => {
+
+  onStaffSelect(staff) {
+    this.setState({
+      selectedStaff: staff,
+    });
+  }
+
+  renderStaffSelected(staff) {
+    if (staff != null) {
       return (
-        <div className="col-xs-12 col-md-6 col-lg-4 ">
-          <div className="staff__item">{staff.name}</div>
+        <div>
+          <h6>Họ và tên: {staff.name}</h6>
+          <p>Ngày sinh: {dateFormat(staff.doB, 'dd/mm/yyyy')}</p>
+          <p>Ngày vào công ty: {dateFormat(staff.startDate, 'dd/mm/yyyy')}</p>
+          <p>Phòng ban: {staff.department.id}</p>
+          <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
+          <p>Số ngày đã làm thêm : {staff.overTime}</p>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  //   Tại sao chỗ này phải đổi thành arrow function
+  renderList = (display) => {
+    return this.props.staffs.map((staff) => {
+      return (
+        <div key={staff.id} className={`col-xs-12 col-md-6 ${display}`}>
+          <div className="staff__item">
+            <p
+              onClick={() => {
+                this.onStaffSelect(staff);
+              }}
+            >
+              {staff.name}
+            </p>
+          </div>
         </div>
       );
     });
+  };
 
+  handleSelect = (event) => {
+    switch (event.target.value) {
+      case '2 cols':
+        this.setState({
+          selectedStaff: null,
+          colDisplay: 'col-lg-6',
+        });
+        console.log(this.state.colDisplay);
+        break;
+
+      case '3 cols':
+        this.setState({
+          selectedStaff: null,
+          colDisplay: 'col-lg-4',
+        });
+        break;
+
+      case '6 cols':
+        this.setState({
+          selectedStaff: null,
+          colDisplay: 'col-lg-2',
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  render() {
     return (
       <div className="staffList container-fluid mt-3">
         <div className="container">
-          <div className="row">{staffList}</div>
+          <div className="row">
+            <select
+              onChange={this.handleSelect}
+              //   value={this.state.value}
+              className="form-select"
+              aria-label="Default select example"
+            >
+              <option defaultValue>3 cột</option>
+              <option value="2 cols">2 cột</option>
+              <option value="3 cols">3 cột</option>
+              <option value="6 cols">6 cột</option>
+            </select>
+          </div>
+          <div className="row">{this.renderList(this.state.colDisplay)}</div>
+          <div className="row">
+            {this.renderStaffSelected(this.state.selectedStaff)}
+          </div>
         </div>
       </div>
     );
