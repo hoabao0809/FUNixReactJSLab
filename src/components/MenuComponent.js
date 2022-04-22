e2a409f99967f6d053830502cc5da8b3d2ca27bf
hoabao0809 <hoabao.0809@gmail.com>
2022-04-22 12:13:37 +0700

    delete unused part

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,31 +1,8 @@
 import { Component } from 'react';
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
