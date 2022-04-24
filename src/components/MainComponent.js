b8d7579c6e16b396892e52d8d78c5f4f0cccf3c6
hoabao0809 <hoabao.0809@gmail.com>
2022-04-24 15:17:01 +0700

    initial commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,44 @@
+import { React, Component } from 'react';
+import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import { DISHES } from './../shared/dishes';
+import Footer from './FooterComponent';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null,
+    };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({
+      selectedDish: dishId,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Menu
+          dishes={this.state.dishes}
+          onClick={(dishId) => this.onDishSelect(dishId)}
+        />
+        <Dishdetail
+          selectedDish={
+            this.state.dishes.filter(
+              (dish) => dish.id === this.state.selectedDish
+            )[0]
+          }
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
