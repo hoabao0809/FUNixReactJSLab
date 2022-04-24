9dc4624cba24c4433cc195823ca076d0ca8af6d6
hoabao0809 <hoabao.0809@gmail.com>
2022-04-24 21:25:22 +0700

    update

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,12 @@
 import { React, Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import Dishdetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import { DISHES } from './../shared/dishes';
+import { COMMENTS } from './../shared/comments';
+import { PROMOTIONS } from './../shared/promotions';
+import { LEADERS } from './../shared/leaders';
 import Footer from './FooterComponent';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
@@ -12,22 +15,22 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      // selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
-
-  // onDishSelect(dishId) {
-  //   this.setState({
-  //     selectedDish: dishId,
-  //   });
-  // }
 
   render() {
     const HomePage = () => {
       return (
-        <Home />
-      )
-    }
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
     return (
       <div>
         <Header />
@@ -38,19 +41,10 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
-        {/* <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        /> */}
-        {/* <Dishdetail
-          selectedDish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        /> */}
+
         <Footer />
       </div>
     );
